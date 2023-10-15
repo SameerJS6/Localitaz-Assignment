2972f64a21749dc2881f158027bad5bfba775057
Sameer Singh <sameersingh1234pok@gmail.com>
2023-10-15 13:14:51 +0530

    Tailwind Config Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        shoeGray: withOpacity("--shoeGray"),
+        shoeDark: withOpacity("--shoeDark"),
+        shoeDarkBrown: withOpacity("--shoeDarkBrown"),
+        shoeBrown: withOpacity("--shoeBrown"),
+        shoeOrange: withOpacity("--shoeLightOrange"),
+        shoeLightOrange: withOpacity("--shoeLightOrange"),
+        shoeBackground: withOpacity("--shoeBackground"),
+      },
+    },
   },
   plugins: [],
 };
