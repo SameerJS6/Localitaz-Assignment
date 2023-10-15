927e60cf261d4aeb37cd9b9743161db7d4e51824
Sameer Singh <sameersingh1234pok@gmail.com>
2023-10-15 13:16:08 +0530

    button component updated

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,11 +16,9 @@ export default function Button({ className, disabled, ...props }: ButtonTypes) {
       disabled={disabled}
       {...props}
       className={twMerge(
-        "bg-zinc-950 text-white rounded-md px-4 py-2",
+        "bg-shoeDark text-shoeBackground rounded-md px-4 py-2",
         className
       )}
-    >
-      Sample Button
-    </button>
+    />
   );
 }
