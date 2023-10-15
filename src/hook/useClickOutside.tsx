4afe5102f5983d18bbf5e545d7284d72ff7f56a1
Sameer Singh <sameersingh1234pok@gmail.com>
2023-10-15 13:15:40 +0530

    useClickOutside hook added

---
+++ src/hook/useClickOutside.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+export function useClickOutside(
+  ref: React.RefObject<HTMLElement>,
+  callback: () => void
+): void {
+  useEffect(() => {
+    const handleClose = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        callback();
+      }
+    };
+    document.addEventListener("mousedown", handleClose);
+
+    return () => document.removeEventListener("mousedown", handleClose);
+  });
+}
