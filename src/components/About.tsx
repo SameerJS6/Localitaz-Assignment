57fa747df44b27bd87a85f27ccc9367406f2b916
SameerJS6 <singh6.sameer@gmail.com>
2024-01-06 22:54:13 +0530

    style:about section complete

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,59 @@
+import Button from "./ui/button";
+
 export default function About() {
-  return <section>About Us</section>;
+  return (
+    <section className="grid place-content-center text-center px-4 my-16 max-w-7xl mx-auto">
+      <div className="grid gap-8">
+        <div className="space-y-4">
+          <h2 className="text-4xl">About Us</h2>
+          <p className="text-balance max-w-[68ch] mx-auto leading-0">
+            Believing neglected so so allowance existence departure in. In
+            design active temper be uneasy. Thirty for remove plenty regard you
+            summer though. He preference connection astonished on of ye.
+          </p>
+        </div>
+        <div className="grid justify-center gap-4">
+          <div className="flex flex-col sm:flex-row gap-4">
+            <div className="max-w-md">
+              <img
+                className="rounded-2xl w-full"
+                src="https://images.unsplash.com/photo-1603191659812-ee978eeeef76?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+            </div>
+
+            <div className="max-w-md">
+              <img
+                className="rounded-2xl"
+                src="https://images.unsplash.com/photo-1603191659812-ee978eeeef76?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col sm:flex-row gap-4">
+            <div className="max-w-md">
+              <img
+                className="rounded-2xl w-full"
+                src="https://images.unsplash.com/photo-1603191659812-ee978eeeef76?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+            </div>
+
+            <div className="max-w-md">
+              <img
+                className="rounded-2xl"
+                src="https://images.unsplash.com/photo-1603191659812-ee978eeeef76?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+
+        <Button className="w-fit lg:w-36 lg:py-3 mx-auto bg-shoeLightOrange">
+          Get Started
+        </Button>
+      </div>
+    </section>
+  );
 }
